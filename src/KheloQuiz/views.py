4c8517da305487b3bbd624d53848a6512ecb7585
Roshan Dalvi <roshandalvi16@gmail.com>
2025-03-26 16:06:39 +0530

    include urls of users app in kheloquiz

--- src/KheloQuiz/views.py
+++ src/KheloQuiz/views.py
@@ -1,28 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')  
+    return render(request, 'layouts/welcome_screen.html')  
 
-def signup(request):
-    return render(request, 'signup.html')
 
-def signin(request):
-    return render(request, 'signin.html')
 
-def reset_password(request):
-    return render(request, 'resetpassword.html')
-
-def dashboard(request):
-    return render(request, 'dashboard.html')
-
-def history(request):
-    return render(request, 'history.html')
-
-def about(request):
-    return render(request, 'about.html')
-
-def setting(request):
-    return render(request, 'setting.html')
 
 
     
